d2d052acc7f016d5abd17776308f82b8c2ae608b
KamilWro <kamil.breczko@gmail.com>
2018-04-10 23:49:16 +0200

    add possibility of extracting n posts

--- src/main/scala/scraper/Scraper.scala
+++ src/main/scala/scraper/Scraper.scala
@@ -5,19 +5,35 @@ import org.jsoup.nodes.{Document, Element}
 import org.jsoup.select.Elements
 import play.api.libs.json.Json
 
+import scala.collection.mutable.ListBuffer
+
 object Scraper {
+  var webPosts = ListBuffer[WebPost]()
+  var length = 0
+  var pageNumber = 0
 
   def main(args: Array[String]) = {
-    val doc = Jsoup.connect("http://bash.org.pl/latest/?page=1").get
-    val post = extract(doc)
-    val value = Json.toJson(post)
-    println(value)
+    val values = extractPosts(10)
+    println(Json.toJson(values))
   }
 
-  def extract(doc: Document): WebPost = {
-    val elements = posts(doc)
-    val element = elements.first()
+  def extractPosts(n: Int): Seq[WebPost] = {
+    while (length < n) {
+      pageNumber += 1
+      val document = getDocument()
+      var posts = getPosts(document)
 
+      var it = posts.listIterator()
+      while (it.hasNext) {
+        webPosts += extract(it.next)
+        length += 1
+      }
+
+    }
+    webPosts
+  }
+
+  def extract(element: Element): WebPost = {
     val id = idText(element)
     val point = pointText(element)
     val content = contentText(element)
@@ -31,7 +47,9 @@ object Scraper {
 
   def pointText(element: Element): String = element.select(".points").text
 
-  def posts(doc: Document): Elements = doc.select(".q.post")
+  def getPosts(doc: Document): Elements = doc.select(".q.post")
+
+  def getDocument(): Document = Jsoup.connect("http://bash.org.pl/latest/?page=" + pageNumber).get
 
 }
 
